bd88c217a903ecd72db408a2ea378d810bbed068
quocanh-0712 <77775464+quocanh-0712@users.noreply.github.com>
2021-07-01 00:45:29 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 	#DATABASE CREDENTIAL
-	$db_host = "localhost";
-	$db_name = "postgres";
-	$db_user = "postgres";
-	$db_password = "1111";
+	$db_host = "ec2-52-23-45-36.compute-1.amazonaws.com";
+	$db_name = "dad36q99j930n6";
+	$db_user = "uwewypgzbkknym";
+	$db_password = "8b15b8a61c0f109b4d6b64270f3ab439af8e719f9f6e857ed922ccc449ec18c9";
 	$conn_string = "host = $db_host port = 5432 dbname = $db_name user = $db_user password = $db_password";
 	$con = pg_connect($conn_string);
 	if(!$con)
